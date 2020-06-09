2be37bb4ebc5287e40e7f0bcaf6bcdbd1847a266
sameer-mitra <49610563+sameer-mitra@users.noreply.github.com>
2020-06-09 21:05:38 +1000

    Added files from Starter App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+// import * as BooksAPI from './BooksAPI'
+import './App.css'
+import ListBooks from './ListBooks.js'
+import SearchBooks from './SearchBooks.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class BooksApp extends React.Component {
+  state = {
+    /**
+     * TODO: Instead of using this state variable to keep track of which page
+     * we're on, use the URL in the browser's address bar. This will ensure that
+     * users can use the browser's back and forward buttons to navigate between
+     * pages, as well as provide a good URL they can bookmark and share.
+     */
+    showSearchPage: false
+  }
+
+  render() {
+    return (
+      <div className="app">
+        {this.state.showSearchPage ? (
+          <SearchBooks />
+        ) : (
+          <ListBooks />
+        )}
+      </div>
+    )
+  }
 }
 
-export default App;
+export default BooksApp
