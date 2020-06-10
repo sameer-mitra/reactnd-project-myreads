7cc3193d92e97039f9a26b07859ab0f1a1350e3f
sameer-mitra <49610563+sameer-mitra@users.noreply.github.com>
2020-06-10 21:02:01 +1000

    Implemented Books List and Move

--- src/App.js
+++ src/App.js
@@ -1,28 +1,79 @@
 import React from 'react'
-import * as BooksAPI from './BooksAPI'
-import { Route } from 'react-router-dom'; 
 import './App.css'
+import { Route } from 'react-router-dom';
+import * as BooksAPI from './BooksAPI'
 import ListBooks from './ListBooks.js'
 import SearchBooks from './SearchBooks.js'
 
 class BooksApp extends React.Component {
+   bookshelves = [
+    { key: 'currentlyReading', name: 'Currently Reading' },
+    { key: 'wantToRead', name: 'Want to Read' },
+    { key: 'read', name: 'Read' }
+  ];
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  }
+    books: [],
+    searchBooks: []
+  };
+  componentDidMount = () => {
+    BooksAPI.getAll().then(books => {
+      this.setState({ books: books });
+    });
+  };
+  moveBook = (book, shelf) => {
+    BooksAPI.update(book, shelf);
 
+    let updatedBooks = [];
+    updatedBooks = this.state.books.filter(b => b.id !== book.id);
+
+    if (shelf !== 'none') {
+      book.shelf = shelf;
+      updatedBooks = updatedBooks.concat(book);
+    }
+
+    this.setState({
+      books: updatedBooks,
+    });
+  };
+  searchForBooks = query => {
+    if (query.length > 0) {
+      BooksAPI.search(query).then(books => {
+        if (books.error) {
+          this.setState({ searchBooks: [] });
+        } else {
+          this.setState({ searchBooks: books });
+        }
+      });
+    } else {
+      this.setState({ searchBooks: [] });
+    }
+  };
+  resetSearch = () => {
+    this.setState({ searchBooks: [] });
+  };
   render() {
+    const { books } = this.state;
     return (
       <div className="app">
-        <Route path="/" exact component={ ListBooks } />
-        <Route path="/search" component={ SearchBooks } />
+        <Route exact path="/"
+          render={() => (
+            <ListBooks
+              bookshelves={this.bookshelves}
+              books={books}
+              onMove={this.moveBook}
+            />
+          )}
+        />
+        <Route path="/search"
+          render={() => (
+            <SearchBooks
+              books={this.searchForBooks}
+              onMove={this.moveBook}
+            />
+          )}
+        />
       </div>
-    )
+    );
   }
 }
 
